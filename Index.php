6d47816754fef98653ac1f506e2cebeb34144e47
MarcEcniv <marcvince43@gmail.com>
2026-02-14 23:27:48 +0800

    Hello v1

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>ATTENDANCE SHEET</h1>
+
+<?php
+echo 'Welcome please input your Time in and Time out';
+?>
+<br><br>
+<?php
+$COLOR = "blue";
+$color = "red";
+echo "My car is $color<br>";
+echo "My house is $COLOR";
+?>
+
+</body>
+</html>*/
